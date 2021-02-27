4ba1c44d406acd96f671019c98a2391dca8b7e49
Jokubas Akramas <j.akramas@gmail.com>
2021-02-27 11:29:26 +0200

    Constants and system csv handlers

---
+++ 1 laboratorinis/Code/constants.py
@@ -0,0 +1,16 @@
+DATASET_TRAIN_FILE: str = 'train_dataset_Jokubas_Akramas.csv'
+OUTPUT_FOLDER_NAME: str = 'output'
+# ----
+CATEGORICAL_DATA_HEADERS = ['REGION', 'OS', 'PROTOCOL']
+CONTINUOUS_DATA_HEADERS = ['X_1', 'X_2', 'X_3', 'X_4', 'X_5', 'X_6', 'X_7']
+OUTPUT_VARIABLE: str = 'MALICIOUS_OFFENSE'
+# ----
+CATEGORICAL_OUTPUT_PATH = OUTPUT_FOLDER_NAME + "/kategorinių_duomenų_analizė.csv"
+CATEGORICAL_OUTPUT_HEADER_NAMES = ['Atributo pavadinimas', 'Kiekis (Eilučių sk.)', 'Trūkstamos reikšmės, %',
+                                   'Kardinalumas', 'Moda', 'Modos dažnumas', 'Moda, %', '2-oji moda',
+                                   '2-osios Modos dažnumas', '2-oji Moda, %']
+CONTINUOUS_OUTPUT_PATH = OUTPUT_FOLDER_NAME + "/tolydinių_duomenų_analizė.csv"
+CONTINUOUS_OUTPUT_HEADER_NAMES = ['Atributo pavadinimas', 'Kiekis (Eilučių sk.)', 'Trūkstamos reikšmės, %',
+                                  'Kardinalumas', 'Minimali reikšmė', '1-asis kvartilis', '3-asis kvartilis',
+                                  'Vidurkis', 'Mediana', 'Standartinis nuokrypis']
+# ----
--- 1 laboratorinis/Code/main.py
+++ 1 laboratorinis/Code/main.py
@@ -1,8 +1,21 @@
 # Python3.8
 # Jokubas Akramas IFF-8/12
 # 1 laboratorinis darbas
-
+# P176B101 Intelektikos pagrindai 2021
 import csv
+import errno
+import os
+import constants as c
+
+
+def create_package_if_no_exist(path):
+    try:
+        os.makedirs(path)
+        print("Output folder created at: ",
+              os.path.dirname(os.path.realpath(path)))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
 
 def csv_to_dict_list(path):
@@ -13,5 +26,14 @@ def csv_to_dict_list(path):
         return list_out, fields
 
 
-dataset, fields = csv_to_dict_list('dataset_Jokubas_Akramas.csv')
-print(fields)
+def write_to_csv(path, items, header_fields):
+    with open(path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=header_fields)
+        writer.writeheader()
+        for row in items:
+            writer.writerow(row)
+
+
+dataset, fields = csv_to_dict_list(c.DATASET_TRAIN_FILE)
+create_package_if_no_exist(c.OUTPUT_FOLDER_NAME)
+write_to_csv(c.CATEGORICAL_OUTPUT_PATH, [], c.CATEGORICAL_OUTPUT_HEADER_NAMES)
