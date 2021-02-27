5f67b78d3c5ef7f7bda9e01b51cdadda1fbd9c71
Jokubas Akramas <j.akramas@gmail.com>
2021-02-27 14:19:31 +0200

    Tolydiniu lenteles analizavimas

--- 1 laboratorinis/Code/main.py
+++ 1 laboratorinis/Code/main.py
@@ -3,7 +3,7 @@
 # 1 laboratorinis darbas
 # P176B101 Intelektikos pagrindai 2021
 from functools import reduce
-
+import numpy as np
 import constants as c
 import handler
 
@@ -19,13 +19,19 @@ def analyse_continuous_data(data):
     csv_list = []
     for i in c.CONTINUOUS_DATA_HEADERS:
         sublist = list(map(lambda x: int(x[i]), data))
+        sublist.sort()
         values = [
             i,  # Atributo pavadinimas
-            len(data),  # Eilučių kiekis
+            len(sublist),  # Eilučių kiekis
             '%d' % (len(list(filter(lambda x: x == '', sublist))) / len(sublist)) + '%',  # Trūkstamos reikšmės
             len(list(set(sublist))),  # Kardinalumas
-            min(sublist), #Minimali reikšmė
-            max(sublist) #Maksimali reikšmė
+            min(sublist),  # Minimali reikšmė
+            max(sublist),  # Maksimali reikšmė
+            np.quantile(sublist, .25),  # 1-asis kvartilis
+            np.quantile(sublist, .75),  # 3-asis kvartilis
+            np.average(sublist),  # Vidurkis
+            np.median(sublist),  # Mediana
+            np.std(sublist)  # Standartinis nuokrypis
         ]
         csv_list.append(append_element_to_headers(c.CONTINUOUS_ANALYSIS_OUTPUT_HEADERS, values))
     handler.write_to_csv(c.CONTINUOUS_OUTPUT_PATH, csv_list, c.CONTINUOUS_ANALYSIS_OUTPUT_HEADERS)
@@ -37,7 +43,7 @@ def analyse_categorical_data(data):
         sublist = list(map(lambda x: x[i], data))
         values = [
             i,  # Atributo pavadinimas
-            len(data),  # Eiluciu kiekis
+            len(sublist),  # Eiluciu kiekis
             '%d' % (len(list(filter(lambda x: x == '', sublist))) / len(sublist)) + '%',  # Trukstamos reiksmes
             len(list(set(sublist)))  # Kardinalumas
         ]
